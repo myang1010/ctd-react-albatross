87586463e50beabee9012b943ed522d717a168c1
myang1010 <myang1010@gmail.com>
2022-07-19 23:18:09 -0700

    practice custom hooks and fragments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
+const useSemiPersistentState = ()=>{
+  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList')));
+
+  useEffect(()=>{
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList))
+  }, [todoList])
+
+  return [todoList, setTodoList]
+}
+
 function App() {
-  
-  const [todoList, setTodoList] = React.useState([]);
+
+  const [todoList, setTodoList] = useSemiPersistentState()
 
   const addTodo=(newTodo)=>{
     setTodoList([...todoList, newTodo])
   }
 
   return (
-    <div>
+    <>
       <h1>Todo List</h1>
-      <TodoList todoList={todoList}/>
       <AddTodoForm onAddTodo={addTodo}/>
-    </div>
+      <TodoList todoList={todoList}/>
+    </>
   );
 }
 
