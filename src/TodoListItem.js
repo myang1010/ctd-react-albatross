e053af877cc4b657b9040c6335b2a08d7dbdb2b0
ming <ming@codethedream.org>
2022-09-13 10:48:50 -0700

    finish first part of assignment

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './TodoListItem.module.css';
 
 function TodoListItem({todo, todo:{title}, onRemoveTodo}){
   return (
-    <li>
+    <li className={style.ListItem}>
       {todo.fields.Title}
       <button onClick={()=>onRemoveTodo(todo.id)}>Remove</button>
     </li>
